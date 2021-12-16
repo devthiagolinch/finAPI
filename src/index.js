da89430e9122a38d8e0fe6a2186c1d879adcbaee
Thiago Linch <devthiagolinch@gmail.com>
2021-12-15 21:28:30 -0400

    Cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
+/** Como estou usando json preciso de um middleware */
+app.use(express.json())
 
-app.post("/new-account", (req, res) => {})
+const custumers = [];
+
+/**
+ * Um conta tera:
+ * cpf - string (recebe do usario)
+ * name - string (recebe do usuario)
+ * id - uid (vamos criar)
+ * statement - []
+ */
+app.post("/new-account", (req, res) => {
+  /* estou usando os {} para desestruturar os dados e definir o que preciso */
+  const { cpf, name } = req.body
+
+  /* Agora preciso começar a codar como será feito o id da conta a ser criarda, usando a dependência UUID */
+  const id = uuidv4()
+
+  /**
+   * Agora que tenho cpf, name e id eu preciso guardar tudo isso em algum lugar, para iss vou usar um BD fake
+   * que seria um array aqui dentro do arquivo mesmo. Esse array se encontra no topo do arquivo, com o nome de
+   * custumers.
+   * Para iserir os dados dentro do array usarei a function de push para inserir algo no array criando tambem um objeto
+   */
+  custumers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+
+  /** Dando tudo certo uso o status 201 + uma mensagem de congratulations */
+  return res.status(201).send("congratulations! Your account has been created!")
+})
 
 app.listen(3333)
